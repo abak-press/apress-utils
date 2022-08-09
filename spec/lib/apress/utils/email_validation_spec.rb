18a6acfc5ea1a5594e29b0e69f1debe8ee6dd214
Ilya Zorin <ilya-zorin@yandex.ru>
2022-08-09 18:40:28 +0700

    feat(email): add cyrillic support for email

--- spec/lib/apress/utils/email_validation_spec.rb
+++ spec/lib/apress/utils/email_validation_spec.rb
@@ -19,10 +19,29 @@ describe Apress::Utils::EmailValidation do
   end
 
   context 'when email is valid' do
-    it_behaves_like('valid email', %w{valid.email@email.com va-l-id_email@email.com validemail@email.com valid.email@e-mail.com valid_email@e.mail.com v3alid_email01@e.mail.com })
+    it_behaves_like(
+      'valid email',
+      %w{
+        моя@почта.рф
+        valid.email@email.com
+        va-l-id_email@email.com
+        validemail@email.com
+        valid.email@e-mail.com
+        valid_email@e.mail.com
+        v3alid_email01@e.mail.com
+        }
+    )
   end
 
   context 'when email is invalid' do
-    it_behaves_like('invalid email', %w{inv&alidemail@email.com invalid.email@e_mail.com inv#alid-email@email.com envalid$email.com invalid@email invalid@email.96})
+    it_behaves_like(
+      'invalid email',
+      %w{
+        inv&alidemail@email.com
+        invalid.email@e_mail.com
+        inv#alid-email@email.com
+        envalid$email.com invalid@email
+        }
+    )
   end
 end
