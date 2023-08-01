c5cb38ef96e162f0a4226d5627e074feed169157
ArtemBorodinEvgenyevich <temych02@gmail.com>
2023-08-01 17:33:16 +0500

    feat: add option to assign error for collection models on validation
    https://jira.railsc.ru/browse/PC4-29910

--- lib/apress/utils/extensions/active_record/validate_uniqueness_in_memory.rb
+++ lib/apress/utils/extensions/active_record/validate_uniqueness_in_memory.rb
@@ -11,20 +11,38 @@ module Apress::Utils::Extensions::ActiveRecord
   module ValidateMethods
     def self.included(model)
       model.class_eval do
-        # Validate that the the objects in +collection+ are unique
-        # when compared against all their non-blank +attrs+. If not
-        # add +message+ to the base errors.
-        def validate_uniqueness_of_in_memory(collection, attrs, message, attribute = :base)
+        # Validate that the objects in collection are unique when compared against all their non-blank attrs.
+        #   If not add message to the base errors.
+        #
+        # options:
+        #   assign_error_to_collection - assign error message to each model in collection
+        #   additional_message - set additional message to :base error for a current model
+        #
+        # Returns Object
+        def validate_uniqueness_of_in_memory(collection, attrs, message, attribute = :base, options = {})
+          duplicates = []
           hashes = collection.inject({}) do |hash, record|
             key = attrs.map {|a| record.send(a).to_s }.join
             if key.blank? || record.marked_for_destruction?
               key = record.object_id
             end
+
+            duplicates.push(record, hash[key]) if hash[key] && options[:assign_error_to_collection]
             hash[key] = record unless hash[key]
+
             hash
           end
+
+          if options[:assign_error_to_collection] && duplicates.present?
+            duplicates.each do |duplicate|
+              duplicate.errors.add(attribute, message)
+            end
+
+            errors.add(:base, options[:additional_message]) if options[:additional_message]
+          end
+
           if collection.length > hashes.length
-            self.errors.add(attribute, message)
+            errors.add(attribute, message)
           end
         end
       end
